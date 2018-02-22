9331ee5e28766dda5059aa8f75ca36ceaf56ea2d
LostSoulFly <lostsoulfly@users.noreply.github.com>
2018-02-21 21:29:50 -0500

    First

---
+++ PubgKillAndRun/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PubgKillAndRun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.pubgStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Miss me with that gay shit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pubgStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 89);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // pubgStatus
+            // 
+            this.pubgStatus.Name = "pubgStatus";
+            this.pubgStatus.Size = new System.Drawing.Size(43, 17);
+            this.pubgStatus.Text = "PUBG: ";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Fuck BlueHole";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel pubgStatus;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PubgKillAndRun/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PubgKillAndRun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            if (IsProcessRunning("TslGame.exe"))
+            {
+                KillProcess("TslGame.exe");
+            }
+
+            System.Threading.Thread.Sleep(1000);
+
+            if (!IsProcessRunning("TslGame.exe"))
+            {
+                LaunchProcess("steam://run/578080", "");
+            } else
+            {
+                MessageBox.Show("PUBG is still running :( try again.");
+            }
+            button1.Enabled = true;
+            
+        }
+
+        public static int LaunchProcess(string exe, string args)
+        {
+            Process p = System.Diagnostics.Process.Start(exe, args);
+
+            return p.Id;
+
+        }
+
+        public static bool IsProcessRunning(string process)
+        {
+            Process[] proc = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(process));
+
+            if (proc.Length == 0)
+                return false;
+            
+            return true;
+        }
+
+        public static bool KillProcess(string proc)
+        {
+            bool cantKillProcess = false;
+
+            //Debug("KillProcess entered: " + proc);
+
+            try
+            {
+                foreach (Process p in Process.GetProcessesByName(Path.GetFileNameWithoutExtension(proc)))
+                {
+                    if (!p.HasExited) p.Kill();
+
+                    p.WaitForExit(2000);    //wait a max of 2 seconds for the process to terminate
+
+                    if (!p.HasExited)
+                        cantKillProcess = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            
+            return !cantKillProcess;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (IsProcessRunning("TslGame.exe"))
+            {
+                pubgStatus.Text = "PUBG: Running";
+            } else
+            {
+                pubgStatus.Text = "PUBG: Not Running";
+            }
+        }
+    }
+}
